12a81d1629597904119017354cb39f1e034b10b4
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2019-05-22 20:55:36 +0200

    Initial commit + gradle

---
+++ src/main/java/com/kapolinarski/szkolenie3/Szkolenie3Application.java
@@ -0,0 +1,13 @@
+package com.kapolinarski.szkolenie3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Szkolenie3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Szkolenie3Application.class, args);
+    }
+
+}
